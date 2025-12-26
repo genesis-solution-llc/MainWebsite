674e8cc734bd7de15d86395b9a075ca6540e23fc
HPlaptopuserr <gantogtokh013@gmail.com>
2025-12-27 03:56:38 +0800

    cards, widgets folders

--- front-end/components/sections/FiveImage.tsx
+++ front-end/components/sections/FiveImage.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 export default function FiveImage() {
     return (
-        <section className="py-12 bg-[#00020F]">
-            <div className="container mx-auto px-4">
-                {/* Add your content here */}
+            <div className="container mx-auto px-4 py-12 bg-[#00020F]">
+                
             </div>
-        </section>
     );
 }
